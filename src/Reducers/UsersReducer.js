48ea805368f3ff24c27ecaf0a9b3ab445fc07a29
jarretbryan <jarret.bryan@gmail.com>
2018-09-01 13:37:34 -0400

    modify reducers

---
+++ src/Reducers/UsersReducer.js
@@ -0,0 +1,21 @@
+
+
+// to fill in properly in a bit
+const initialState = {
+    // user: null,
+    // loggedIn: false,
+    // authenticated: false,
+    // loginSuccess: false,
+    // error: null
+
+}
+
+
+const usersReducer = (state=initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+export default usersReducer
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from 'react-router-dom' 
+import usersReducer from './Reducers/UsersReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(usersReducer)
+
+console.log(store.getState())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
