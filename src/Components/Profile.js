33002d11e57f0d6d180f9261130b9d25f6cc6fa0
jarretbryan <jarret.bryan@gmail.com>
2018-09-02 17:05:28 -0400

    add in show profile if logged in logic

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PlaylistContainer from './PlaylistContainer';
 import Player from './Player';
+import { Redirect} from 'react-router-dom'
 
 
 class Profile extends Component {
+    
+    showProfile = () => {
+        if (!this.props.loggedIn){
+            return <Redirect to="/" />
+        } else {
+            return (
+                <div id='profile'>
+                    <header>
+                        <img src={this.props.user.profile_image} alt={this.props.username} />
+                        <h4>{this.props.user.username}</h4>
+
+                    </header>
+                    <p>hey let's listen to some podcasts this is your profile!</p>
+                    <Player />
+                    <PlaylistContainer />
+
+                </div>
+            );
+        }
+    }
+    
     render() {
-        return (
-            <div id='profile'>
-                <header>
-                    <img src={this.props.user.profile_image}alt={this.props.username} />
-                    <h4>{this.props.user.username}</h4>
-                    
-                </header>
-                <p>hey let's listen to some podcasts this is your profile!</p>
-                <Player />
-                <PlaylistContainer />
-                
-            </div>
-        );
+        return(
+            <Fragment>
+                {this.showProfile()}
+            </Fragment>
+        )
     }
 }
 
-const mapStateToProps = ({ usersReducer: { user } }) => ({
-    user
+const mapStateToProps = ({ usersReducer: { user, loggedIn } }) => ({
+    user,
+    loggedIn
 })
 
 export default connect(mapStateToProps)(Profile);
