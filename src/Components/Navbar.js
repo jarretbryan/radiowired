c1539445d5556cbedbc160ef38cd350d6cc0df70
jarretbryan <jarret.bryan@gmail.com>
2018-09-12 14:09:12 -0400

    cleanup navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom' 
 import { logoutUser } from '../Actions/index';
-import { Menu, Button, Search, Label, Image, Icon } from 'semantic-ui-react'
-import SearchBox from './SearchBox';
+import { Menu, Label, Image, Icon } from 'semantic-ui-react'
+// import SearchBox from './SearchBox';
 // omitted searchBox because of time constraints
 
 
@@ -12,17 +12,17 @@ class Navbar extends Component {
     logButton = () => {
         if (!localStorage.loggedIn){
             return(
-               
                     <Label size="large" color="black">
                         <NavLink to='/login'>Login</NavLink>
                     </Label> 
             ) 
         } else {
-            return <Label size="large" color="black" onClick={this.logOut}> 
+            return(
+            <Label size="large" color="black" onClick={this.logOut}> 
                 <NavLink to='/' >
                     LogOut
                 </NavLink>
-            </Label>
+            </Label>)
         }
     }
 
@@ -33,14 +33,13 @@ class Navbar extends Component {
             <Label color='blue' image>
                 <Image avatar spaced="right" src={this.props.user.profile_image} />
                 {this.props.user.username}
-                        <Label.Detail> 
-                            <Icon fitted name='podcast' /> 
-                        </Label.Detail>
+                <Label.Detail> 
+                    <Icon fitted name='podcast' /> 
+                </Label.Detail>
             </Label>
             </Menu.Item>  )
         }
     }
-
 
     // searchField = () => {
     //     if (!!localStorage.loggedIn){
@@ -55,8 +54,8 @@ class Navbar extends Component {
     // }
     
     logOut = () => {
-            localStorage.clear()
-            this.props.logoutUser()
+        localStorage.clear()
+        this.props.logoutUser()
     }
 
     logoLink = () => {
