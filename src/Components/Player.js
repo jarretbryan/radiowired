8c3444b2bb7657909e2a819ae11166dde05e151f
jarretbryan <jarret.bryan@gmail.com>
2018-09-12 14:23:16 -0400

    cleanup player

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,9 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import StreamAdapter from '../api/StreamAdapter';
 import radio from '../gifs/Radio-1.2s-200px.gif';
-// import audio from {this.state.audio}
 import ReactAudioPlayer from 'react-audio-player';
-import { Card, Sidebar, Image, Divider, Menu } from 'semantic-ui-react'
+import { Sidebar, Image, Divider, Menu } from 'semantic-ui-react'
 
 
 
@@ -27,8 +26,6 @@ class Player extends Component {
             audio: res.latest_episode['audio'],
             audio_length: res.latest_episode['audio_length']   
         }))
-        // this will have a fetch GET request to my API, which wil get from the third party api 
-        //streamAdapter.get_episodes(this.props.streamID).then(() => this.setState......)
     }
 
     componentDidUpdate(){
@@ -40,7 +37,6 @@ class Player extends Component {
     }
 
     componentWillUnmount(){
-        // this will have a fetch delete request which will just delete everything on my backend with this stream id
         StreamAdapter.delete_episodes()
     }
 
@@ -49,41 +45,31 @@ class Player extends Component {
             return <img src={radio} alt="loading" />
         } else {
             return (
-               
-                <Sidebar as={Menu} inverted vertical animation='overlay' direction='right' visible={true}  >
-                 
-                  
+                <Sidebar as={Menu} inverted vertical animation='overlay' direction='right' visible={true}  > 
                 <Menu.Item>
                         <h1>{this.props.streamTitle}</h1>
                         <h4>{this.state.episode_title}</h4>
-                </Menu.Item>       
-                    
+                </Menu.Item>             
                 <Menu.Item>
-
                     <Image src={this.props.thumbnail} centered /> 
                 </Menu.Item>
-                <Menu.Item>
-
-                  
+                <Menu.Item>          
                     <ReactAudioPlayer
                         src={this.state.audio} 
                         autoPlay
                         controls
                         crossorigin="use-credentials"
                         />
-                        <Divider />
+                <Divider />
 
-                        <p>{this.state.description}</p>
-                    </Menu.Item>
-               
+                    <p>{this.state.description}</p>
+                </Menu.Item>
+     
                <Menu.Item>
-
-                <h6> Audio Not Playing? Some Podcast Audio is designed to only be accessible from the publisher's website - if it doesn't play after a few seconds, we might not have access to it. Sorry!</h6>
+                    <h6> Audio Not Playing? Some Podcast Audio is designed to only be accessible from the publisher's website - if it doesn't play after a few seconds, we might not have access to it. Sorry!</h6>
                </Menu.Item>
-              
 
-                </Sidebar>
-                
+                </Sidebar>          
             )
         }
     }
