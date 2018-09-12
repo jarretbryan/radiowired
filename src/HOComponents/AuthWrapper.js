0263ffcec6e6e49d6b973dad192f9ba2b62318a9
jarretbryan <jarret.bryan@gmail.com>
2018-09-12 14:21:09 -0400

    cleanup AuthWrapper

--- src/HOComponents/AuthWrapper.js
+++ src/HOComponents/AuthWrapper.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
-// import * as actions from '../actions'
 import  * as actions from '../Actions/index';
 import radio from '../gifs/Radio-1.2s-200px.gif'; 
-import { authenticatingUser } from '../Actions/index';
 
 
 
 const AuthWrapper = WrappedComponent => {
     class AuthorizedComponent extends React.Component {
         componentDidMount() {
-            // POTENTIAL SECURITY FLAW!!! my tokens don't expire
             if (localStorage.getItem('jwt') && !this.props.loggedIn) this.props.fetchCurrentUser()
         }
 
         render() {
-            // debugger;
             if (localStorage.getItem('jwt') && this.props.loggedIn) {
                 return <WrappedComponent />
             } else if (localStorage.getItem('jwt') && this.props.checkingAuth) {
-                return <img src={radio} />
+                return <img src={radio} alt="loading" />
             } else {
                 return <Redirect to="/" />
             }
